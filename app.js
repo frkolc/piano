b3f90c85ba6c290f65705c245eb7fadf6d8a9099
frko.lc <frko.lc97@gmail.com>
2023-05-16 03:32:30 +0200

    creacion de archivo app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(
+    button => button.addEventListener('click', playSound)
+);
+
+
+
+function playSound(event) {
+    const button = event.target;
+    const note = button.dataset.note;
+
+
+   const audio = document.getElementById(`audio${note}`);
+
+
+   audio.pause();
+   audio.currentTime = 0;
+   audio.play();
+}
+document.addEventListener('keydown', event => {
+    const key = event.key;
+    const button = document.querySelector(`button[data-key="${key}"]`)
+    
+    if (button)
+        button.click();
+});
